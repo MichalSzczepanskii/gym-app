5fb500295ecc73653e23eeebb34e0402668cee0b
Michał <42086467+MichalSzczepanskii@users.noreply.github.com>
2021-10-17 18:31:48 +0200

    PRO login page finished

---
+++ src/resources/views/login.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.default')
+@section('content')
+    <div class="pt-6 px-7 pb-10 md:flex md:justify-center lg:p-0 lg:items-center lg:h-full">
+        <div class="md:w-3/5 md:shadow-2xl md:px-7 md:py-6 md:border md:border-gray-300 md:rounded-xl md:my-10
+        lg:w-2/4 lg:border-0 lg:shadow-none lg:px-28 lg:py-0 lg:my-0 lg:max-w-3xl lg:min-w-max">
+            <div class="text-center pt-5 pb-9 lg:pb-14">
+                <h2 class="font-semibold text-2xl lg:text-4xl">Sign in to your account</h2>
+            </div>
+            <form action="">
+                <div class="py-3">
+                    <label for="email">Email address</label>
+                    <input type="text" name="login" id="email"
+                           class="block border border-gray-300 rounded-md px-3 py-2 w-full">
+                </div>
+                <div class="py-3">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password"
+                           class="block border border-gray-300 rounded-md px-3 py-2 w-full">
+                </div>
+                <div class="flex justify-between py-5">
+                    <div>
+                        <label>
+                            <input type="checkbox" class="h-4 w-4 border-gray-300 rounded"> Remember me
+                        </label>
+                    </div>
+                    <a href="#" class="block text-purple-700 hover:text-purple-400">Forgot your password?</a>
+                </div>
+                <div class="py-4">
+                    <button class="w-full py-3 border-2 border-purple-700 rounded-full text-purple-700
+                    hover:border-transparent hover:text-white hover:bg-purple-700">Login</button>
+                </div>
+            </form>
+        </div>
+        <div class="hidden lg:block w-full h-full" style="background: url('{{asset('img/gym.jpg')}}') center center; background-size: cover">
+            &nbsp;
+        </div>
+    </div>
+@endsection
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,107 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.default')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <style>
-            html, body {
-                width: 100%;
-                height: 100%;
-            }
-            body {
-                font-family: 'Poppins', 'sans-serif';
-            }
-            #welcome {
-                background: url({{asset('img/welcome1.jpg')}}) fixed no-repeat;
-                background-size: 240%;
-                background-position: 56% 0%;
-            }
-            #motivation {
-                background: url({{asset('img/welcome3.jpg')}}) no-repeat;
-                background-size: 120%;
-                background-position: 40% 120%;
-            }
-            .background-text h2, .background-text p{
-                text-shadow: black 0 0 15px;
-            }
-            .mainColorSVG{
-                filter: invert(17%) sepia(70%) saturate(3468%) hue-rotate(257deg) brightness(105%) contrast(103%);
-            }
-            #mobileMenu {
-                transition: transform 0.2s;
-            }
-            .hideMenu {
-                transform: translateY(-100%);
-            }
-            @media (min-width: 768px) {
-                #motivation {
-                    background-size: 110%;
-                    background-position: 40% 65%;
-                }
-                #welcome {
-                    background-size: 120%;
-                    background-position: 56% 0%;
-                }
-            }
-            @media (min-width: 1024px) {
-                #welcome {
-                    background-size: cover;
-                    background-position: 56% 0%;
-                }
-                #motivation {
-                    background-size: cover;
-                    background-position: 0 60%;
-                }
-            }
-        </style>
-    </head>
-    <body>
-    <!-- This example requires Tailwind CSS v2.0+ -->
-    <nav class="bg-purple-600 flex items-center px-6 flex-col relative z-10 lg:px-20">
-        <div class="flex w-full justify-between min-h-20 py-5">
-            <div class="flex items-center">
-                <h2 class="text-white text-3xl font-bold lg:text-4xl">Good<span class="text-purple-300">lyfe</span></h2>
-            </div>
-            <div class="cursor-pointer lg:hidden" id="hamburger" onclick="toggleMenu()">
-                <img src="{{asset('img/icon/menu.svg')}}" alt="menu"/>
-            </div>
-            <div class="hidden lg:flex w-full justify-center">
-                <ul class="text-white text-center min-w-min w-5/12 flex">
-                    <li class="p-3 text-lg min-w-max w-1/5 hover:bg-purple-800 rounded-3xl"><a href="#" class="px-5">Classes</a></li>
-                    <li class="p-3 text-lg min-w-max w-1/5 hover:bg-purple-800 rounded-3xl"><a href="#" class="px-5">Timetable</a></li>
-                    <li class="p-3 text-lg min-w-max w-1/5 hover:bg-purple-800 rounded-3xl"><a href="#" class="px-5">Clubs</a></li>
-                    <li class="p-3 text-lg min-w-max w-1/5 hover:bg-purple-800 rounded-3xl"><a href="#" class="px-5">Nutrition</a></li>
-                    <li class="p-3 text-lg min-w-max w-1/5 hover:bg-purple-800 rounded-3xl"><a href="#" class="px-5">Free Trial</a></li>
-                </ul>
-            </div>
-            <div class="hidden lg:flex justify-center items-center ">
-                <a href="#" class="py-3 px-14 bg-white text-purple-700 rounded-full hover:bg-gray-200 hover:border-4 hover:border-purple-500 border-transparent border-2 hover:border-current">Login</a>
-            </div>
-        </div>
-    </nav>
-    <div class="w-full px-8 bg-purple-800 absolute hideMenu" id="mobileMenu">
-        <ul class="text-white">
-            <li class="py-3 text-lg w-32"><a href="#" class="w-full h-full block">Classes</a></li>
-            <li class="py-3 text-lg w-32"><a href="#" class="w-full h-full block">Timetable</a></li>
-            <li class="py-3 text-lg w-32"><a href="#" class="w-full h-full block">Clubs</a></li>
-            <li class="py-3 text-lg w-32"><a href="#" class="w-full h-full block">Nutrition</a></li>
-            <li class="py-3 text-lg w-32"><a href="#" class="w-full h-full block">Free Trial</a></li>
-        </ul>
-    </div>
+@section('content')
     <div id="welcome" class="background-text h-2/4 md:h-2/5 lg:h-4/5 flex flex-col justify-center gap-y-9 items-center">
         <div class="text-center">
             <h2 class="font-bold text-white text-5xl md:text-6xl lg:text-7xl">Welcome to<br/> Goodlyfe Gyms</h2>
         </div>
         <a href="#" class="bg-purple-600 text-white block px-8 py-4 rounded-full text-lg md:text-xl
         lg:px-10 lg:py-6 border-2 border-transparent hover:border-purple-700 hover:bg-white hover:text-purple-700">
-           See the benefits
+            See the benefits
         </a>
     </div>
     <div class="my-7 lg:flex lg:flex-row lg:justify-center lg:items-center lg:my-32">
@@ -211,66 +117,4 @@
             </a>
         </div>
     </div>
-
-    <footer class="bg-gray-900 pt-12 px-12 md:flex md:flex-wrap lg:px-32 lg:justify-between">
-        <div class="my-5 md:w-2/4 md:px-5 lg:w-1/4">
-            <a href="#">
-                <h2 class="font-bold text-white text-4xl">Good<span class="text-purple-700">lyfe</span></h2>
-            </a>
-            <p class="text-white leading-7 text-justify py-5">Lorem ipsum dolor amet, consectetur adipiscing elit. Eget
-                nisl nunc quam ac sed turpis volutpat. Cursus sed massa non nisi, placerat.</p>
-        </div>
-        <div class="my-7 md:w-2/4 md:px-7 lg:w-1/4">
-            <h2 class="text-white text-2xl">Quick Links</h2>
-            <ul class="my-6 list-none">
-                <li class="text-white text-lg my-5 font-light hover:text-purple-400"><a href="#">Classes</a></li>
-                <li class="text-white text-lg my-5 font-light hover:text-purple-400"><a href="#">Timetable</a></li>
-                <li class="text-white text-lg my-5 font-light hover:text-purple-400"><a href="#">Clubs</a></li>
-                <li class="text-white text-lg my-5 font-light hover:text-purple-400"><a href="#">Nutrition</a></li>
-                <li class="text-white text-lg my-5 font-light hover:text-purple-400"><a href="#">Free Trial</a></li>
-            </ul>
-        </div>
-        <div class="my-16 md:w-2/4 md:px-7 lg:w-1/4">
-            <h2 class="text-white text-2xl">Reach us</h2>
-            <ul class="my-6 list-none">
-                <li class="text-white text-lg my-5 font-light hover:text-purple-400"><a href="#"><img src="{{asset('img/icon/email.svg')}}" class="w-7 mainColorSVG float-left mr-4" alt="email"><span class="leading-6">admin@goodlyfe.com</span></a></li>
-                <li class="text-white text-lg my-5 font-light hover:text-purple-400"><a href="#"><img src="{{asset('img/icon/phone.svg')}}" class="w-7 mainColorSVG float-left mr-4" alt="email"><span class="leading-6">+88 98765 43210</span></a></li>
-                <li class="text-white text-lg my-5 font-light hover:text-purple-400"><a href="#"><img src="{{asset('img/icon/location.svg')}}" class="w-7 mainColorSVG float-left mr-4" alt="email"><span class="leading-6">90210 Beverly Hills California USA</span></a></li>
-            </ul>
-        </div>
-        <div class="text-white font-light text-center pt-5 pb-7 md:flex-shrink-0 md:w-full lg:flex lg:flex-row-reverse lg:justify-between">
-            <div>
-                <a href="#" class="hover:text-purple-400">Terms & Conditions</a> <span class="md:px-1.5">|</span>
-                <a href="#" class="hover:text-purple-400">Privacy Policy</a> <span class="hidden md:inline md:px-1.5">|</span>
-                <a href="#" class="hover:text-purple-400">Sitemap</a> <span class="md:px-1.5">|</span>
-                <a href="#" class="hover:text-purple-400">Disclaimer</a>
-            </div>
-            <div class="mt-5 lg:mt-0">
-                &copy; {{now()->year}} GoodLyfe. All Rights reserved
-            </div>
-        </div>
-    </footer>
-    <script>
-        const hamburger = document.getElementById('hamburger');
-        const mobileMenu = document.getElementById('mobileMenu');
-        const menuItems = document.querySelectorAll('#mobileMenu > ul > li');
-        console.log(menuItems);
-        menuItems.forEach((item) => {
-            item.addEventListener('click', toggleMenu)
-        })
-
-        function toggleMenu() {
-            if(mobileMenu.classList.contains('hideMenu')) {
-                mobileMenu.classList.remove('hideMenu');
-                hamburger.querySelector('img').src = '{{asset('img/icon/close.svg')}}';
-            }else {
-                mobileMenu.classList.add('hideMenu');
-                hamburger.querySelector('img').src = '{{asset('img/icon/menu.svg')}}';
-            }
-        }
-        document.addEventListener('click', (e) => {
-            console.log(e.target.id);
-        })
-    </script>
-    </body>
-</html>
+@endsection
